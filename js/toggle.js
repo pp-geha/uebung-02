6e21717a6d10ff5c4262ab045b5bd304a487e0a2
Hälterlein Gerhard <Hlterlein@wkm.webagentur.de>
2018-11-29 09:20:31 +0100

    first try

---
+++ js/toggle.js
@@ -0,0 +1,10 @@
+
+
+function menu_toggle() {
+  var x = document.getElementById('menu_wrap');
+  if (x.style.display === "block") {
+      x.style.display = "none";
+  } else {
+      x.style.display = "block";
+  }
+}
