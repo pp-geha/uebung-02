79eb3b52631c31a17e3b8692e4cd00e0bf52de8d
Hälterlein Gerhard <Hlterlein@wkm.webagentur.de>
2019-02-05 15:41:37 +0100

    html slide js, css slide, css leckeres

---
+++ js/slickslider.js
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+  $(document).ready(function(){
+    $('.slideshow section').slick({
+      rtl: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: int(ms) 5000;
+    });
+  });
+</script>
